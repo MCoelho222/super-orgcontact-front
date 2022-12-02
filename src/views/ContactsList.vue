452ee123dd7b64890b74241a93ac6b4082b290ba
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-12-02 15:01:52 -0300

    feat: select bar working

--- src/views/ContactsList.vue
+++ src/views/ContactsList.vue
@@ -1,13 +1,40 @@
 <template>
-    <div class="container-fluid">
-        <div id="top-msg">
+    <div class="container-fluid p-4">
+        <div id="top-msg" class="container-fluid">
             <h5 id="welcome">{{ welcomeMsg }}</h5>
-            <p id="words">{{ someOtherWords }}</p>
+            <p v-if="!isEmpty" id="words">{{ someOtherWords }}</p>
             <p id="email">{{ emailAddress }}</p>
         </div>
-
-        <p id="sorry-msg" v-if="isEmpty">Sorry, you don't have any contacts to share...</p>
-        <p id="todo-msg" v-if="isEmpty">Please, go to your e-mail box and add some e-mails to your contacts.</p>
+            <div id="select-div" class="container-fluid p-4">
+                <div id="in-select-div"  class="row mb-3">
+                    <div class="col-4">
+                        <label>
+                        <h6 id="select-bar-title" class="form-label">Select a domain:</h6>
+                        </label>
+                        <select 
+                        id="select-bar"
+                        class="form-select shadow"
+                        @input="selectDomain"
+                        ><option disabled selected>
+                        {{ selected }}
+                        </option>
+                        <option>all domains</option>
+                        <option 
+                        v-for="(domain, key, index) in contacts"
+                        :value="key"
+                        :key="index">
+                        {{ key }}
+                        </option>
+                        </select>
+                    </div>
+                
+                </div>
+            </div>
+       
+        <p id="sorry-msg" v-if="isEmpty">Whoops! It seems you don't have many e-mail contacts in your account...</p>
+        <p id="todo-msg" v-if="isEmpty">No problem! Please, go to your e-mail account and add some e-mails to your contacts. Alternatively, try to login with another account.</p>
+        <p id="alternative-msg" v-if="isEmpty">Alternatively, try to login with another account.</p>
+        <img id="lucky-cat" src="../assets/Lucky-Beckoning-Cat.png" alt="Lucky cat" v-if="isEmpty">
         <table class="table align-middle" v-else>
             <thead>
                 <tr>
@@ -20,7 +47,6 @@
                 <tr v-for="(domain, key, index) in peopleObj.contacts" :key="index">
                 <th scope="row">{{index + 1}}</th>
                     <td class="text-start">{{ key }}</td>
-
                     <td class="text-start">
                         <ul>
                             <li v-for="(email,  item) in domain" :key="item">{{email}}</li>
@@ -32,6 +58,7 @@
     </div>
 </template>
 <script>
+import { isArray } from "@vue/shared";
 import { useCookies } from "vue3-cookies";
 import { mapActions } from 'vuex';
 
@@ -42,56 +69,64 @@ export default {
         return {
             peopleObj: {
                 'profile': {},
-                'contacts': []
+                'contacts': {}
             },
             name: 'User',
-            email: 'useremail@email.com' 
+            email: 'useremail@email.com' ,
+            selected:'all domains',
+            contacts: {},
+            domains: []
         }
     },
     methods: {
-        ...mapActions(["contacts/getPersonInfo", "auth/logout"]),
+        ...mapActions(["auth/validateToken", "auth/logout"]),
         async populate() {
             let token = cookies.get('token')
-            if (token !== null && token.status) {
+            if (token !== null) {
                 let loader = this.$loading.show();
-                let tokenKeys = Object.keys(token)
-                if (tokenKeys.length <= 2) {
-                    await this["contacts/getPersonInfo"](token.token).then(() => {
-                        let verifiedToken = cookies.get('token')
-                        if (verifiedToken !== null && verifiedToken.status) {
-                            verifiedToken.reload = true
-                            let newToken = JSON.stringify(verifiedToken)
-                            cookies.set('token', newToken)
-                            let people = localStorage.getItem('people')
-                            if (people != null) {
-                                let peopleObj = JSON.parse(people)
-                                this.peopleObj = peopleObj
-                                let parsePeople = JSON.parse(people)
-                                this.name = parsePeople['profile']['name']
-                                this.email = parsePeople['profile']['email']
-                            }
-                            loader.hide()
-                        } else {
-                            this["auth/logout"]().then(() => {
-                                loader.hide()
-                                this.$router.push("/")
-                            })
-                        }
-                    })
-                } else {
-                    let people = localStorage.getItem('people')
-                    if (people != null) {
-                        let peopleObj = JSON.parse(people)
-                        this.peopleObj = peopleObj
-                        let parsePeople = JSON.parse(people)
-                        this.name = parsePeople['profile']['name']
-                        this.email = parsePeople['profile']['email']
-                        loader.hide()
+                await this["auth/validateToken"](token.token).then(() => {
+                    loader.hide()
+                    // check valid token
+                    let verifiedToken = cookies.get('token')
+                    if (verifiedToken.status) {
+                        // Populate contacts object
+                        let people = localStorage.getItem('people')
+                        if (people != null) {
+                            let peopleObj = JSON.parse(people)
+                            this.peopleObj = peopleObj
+                            this.contacts = peopleObj.contacts
+                            let parsePeople = JSON.parse(people)
+                            this.name = parsePeople['profile']['name']
+                            this.email = parsePeople['profile']['email']
+                        } 
+                    } else {
+                        this["auth/logout"]().then(() => {
+                            this.$router.push("/") 
+                        })
                     }
-                }
+                })
             }
             if (token == null || !token.status) {
                 this.$router.push("/") 
+            }
+        },
+        selectDomain() {
+            let people = localStorage.getItem('people')
+            if (people != null) {
+                let peopleObj = JSON.parse(people)
+                this.peopleObj.contacts = peopleObj.contacts
+            }
+            let selectBar = document.getElementById('select-bar')
+            let selectVal = selectBar.value
+            this.selected = selectVal
+            let entries = Object.entries(this.peopleObj.contacts)
+            let filtered = entries.filter(([key, value]) =>
+                key == this.selected && isArray(value)
+            )
+            let filteredObj = Object.fromEntries(filtered)
+            this.peopleObj.contacts = filteredObj
+            if (this.selected == 'all domains') {
+                this.peopleObj.contacts = this.contacts
             }
         }
     },
@@ -121,18 +156,9 @@ export default {
   margin: 0;
   padding: 0;
 }
-
 ul {
     list-style-type: none;
 }
-table {
-    background-color: white;
-}
-
-.container-fluid {
-    background-color: aliceblue
-}
-
 #top-msg {
     color: rgb(34, 33, 33);
 }
@@ -142,6 +168,9 @@ table {
 }
 
 #todo-msg {
+    padding-top: 10px;
+}
+#alternative-msg {
     padding-top: 10px;
 }
 
@@ -157,5 +186,28 @@ table {
     margin: 10px 10px;
     color: rgb(9, 123, 236);
     font-weight: bolder;
+}
+#select-div {
+    text-align: left;
+}
+
+#select-bar {
+    margin: 5px 0px 30px 0px;
+    padding: 5px 0px 5px 10px;
+
+    /* width:80% */
+}
+#select-bar-title {
+    font-weight: bold;
+}
+select:hover {
+    
+    border-color: dodgerblue;
+    cursor: pointer;
+}
+#lucky-cat {
+    margin-top: 50px;
+    height: 150px;
+    width: 150px;
 }
 </style>
