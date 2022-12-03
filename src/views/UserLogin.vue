6399d109a97736ca599d5bb772330a27642c4be5
Marcelo Coelho <92119579+MCoelho222@users.noreply.github.com>
2022-12-02 22:34:34 -0300

    refact: userlogin-view cleaner code

--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -1,28 +1,29 @@
 <template>
-    <div id="home">
+    <div id="template">
         <div class="side-bar p2" :style="sidebar">
-            
+            <!-- SIDEBAR TEXT -->
             <div id="side-bar-text">
                 <p v-if="toggleSideBar">
                 Welcome, <br> <br>
                 In this application you will be able to analyze your e-mail contacts in an organized way. When you login to the app, the e-mails associated to your contacts will be recorded in a database and displayed in a table. The contacts will be separated by domain, in a clearer way, so that you can make better strategies about communication with your clients. Also, you will have access to a piechart which shows you the distribution by domain. <br> We hope you enjoy it, and invite your friends to join us! In the next versions, we plan to let you download the table in an excel file, make more specific searches in the database, and much more. <br> So... Enjoy it! <br><br> Sincerely, <br> Super OrgContact
                 </p>
             </div>
-            <!-- div geral do switch -->
+            <!-- SWITCH DIV-->
             <div id="switchDiv">
-                <!-- body do switch -->
+                <!-- SWITCH BODY  -->
                 <div id="switchBody" @click="toggleSwitch">
-                    <!-- Switch circle -->
+                    <!-- SWITCH CIRCLE -->
                     <div id="switchCircle"></div>
                 </div>
             </div>
-            
         </div>
-
+        <!-- LOGIN -->
         <div id="login-div" :style="main">
+            <!-- IMAGE -->
             <div id="people-img-div">
                 <img id="people-img" src="../assets/people4.jpeg" alt="people image">
             </div>
+            <!-- ENTER WITH GOOGLE -->
             <div id="login-main-div">
                 <img id="brand-img" src="../assets/at-sign-aroba-email-e-mail-47390.webp" alt="arroba symbol">
                 <h4 id="brand-login">Super OrgContacts</h4>
@@ -34,6 +35,7 @@
                 type="button"
                 @click="enterWithGoogle">Login with Google</button>
             </div>
+            <!-- FOOTER -->
             <p id="footer">&copy; 2022 Super OrgContacs. All rights reserved.</p>
         </div>
     </div>
@@ -65,19 +67,18 @@ export default {
             let switchCircle = document.getElementById("switchCircle")
             let switchDiv = document.getElementById("switchDiv")
 
-            // Se não tiver classe switchCircle, adiciona
+            // Add switchCircle class, if not present
             if (switchCircle.className != "switchCircle") {
             switchCircle.className = "switchCircle"
             switchDiv.style.justifyContent = "center"
 
             } else {
-            // Se houver, apaga
+            // If present, delete
             switchCircle.className = ""
             switchDiv.style.justifyContent = "right"
             }
             this.$store.commit("template/controlSideBar")
             },
-            
     },
     computed: {
         ...mapState({
@@ -89,7 +90,7 @@ export default {
         main() {
             return this.$store.getters['template/main']
         },
-        // Controla o sideBar e tipos de botões
+        // Controls the sidebar
         toggleSideBar() {
         return this.$store.state.template.toggleSideBar
         }
@@ -111,64 +112,55 @@ export default {
     height: 60px;
     margin-bottom: 20px;
 }
+/* Enter with Google */
 #login-btn-google {
     font-size: large;
     margin-bottom: 200px;
     margin-top: 20px;
 }
-
-#home {
-    
+/* Main div */
+#template { 
     display: flex;
 }
+/* People image div */
 #people-img-div {
-    padding-top: 100px;
-    
+    padding-top: 100px;  
 }
 #people-img {
-
     width: 300px;
     height: 200px;
 }
+/* General login div. Includes people image brand img enter with google */
 #login-div {
-    /* position: relative; */
     min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    /* padding: 180px 30px; */
 }
-
+/* @ */
 #brand-login {
     color: rgb(39, 39, 39);
 }
-
+/* Phrases above login button */
 #words-login {
     color: rgb(29, 90, 244);
 }
 #footer {
     font-size: x-small;
     color: rgb(87, 84, 84);
-    /* margin-top: 300px; */
-    /* text-align: center;
-    justify-content: center; */
-    /* position: absolute; */
-    /* bottom: 0px; */
-    /* width: 90%; */
-    /* height: 1.5rem; */
     margin-bottom: 5px;
 
 }
-/* Div geral do switch */
+/* Switch general div */
 #switchDiv {
   display: flex;
   justify-content: flex-end;
 }
-/* Corpo do switch, alinha o switch para esquerda */
+/* Switch body, align left */
 #switchBody {
   display: flex;
-  justify-content: left; /* alinha na esquerda */
-  align-items: center; /* alinha na vertical */
+  justify-content: left;
+  align-items: center;
   width: 50px;
   height: 28px;
   border: 1px solid rgb(247, 248, 251);
@@ -183,11 +175,11 @@ export default {
   border-radius: 50%;
   transition: all 0.4s;
 }
-/* Ao clicar adiciona esta classe, que move para direita */
+/* On click add this class, which moves to the right */
 .switchCircle {
   margin-left: 24px !important;
 }
-/*Div geral do sidebar*/
+/*Sidebar general div*/
 .side-bar {
   position: sticky;
   top: 0;
@@ -200,5 +192,4 @@ export default {
     text-align: justify;
     color: whitesmoke;
 }
-
 </style>
